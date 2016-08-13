b5ebaca67672ce4f6a6be0dee0daed74645e24b7
Edward Mason <esmason66@gmail.com>
2016-08-13 14:27:32 -0700

    db now has all changes of other non-staging branches ready for pr

--- music_this_week_app/models.py
+++ music_this_week_app/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    spotify_uri = models.CharField(max_length= 100, primary_key = True)
+
+    def __str__(self):
+        return self.spotify_uri
